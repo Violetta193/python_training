5046ff3631cb35f7db4c93bf7d7d7f754088c2a4
Violetta Kazantseva <kazanova1931@gmail.com>
2023-06-05 16:16:22 +0300

    Выделены вспомогательные методы и классы

--- test_addgroup.py
+++ test_addgroup.py
@@ -5,7 +5,8 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import Select
 from selenium.common.exceptions import NoSuchElementException
 from selenium.common.exceptions import NoAlertPresentException
-import unittest, time, re
+import unittest
+from group import Group
 
 
 
@@ -13,34 +14,57 @@ class TestAddGroup(unittest.TestCase):
     def setUp(self):
         self.wd = webdriver.Firefox()
         self.wd.implicitly_wait(30)
-
-    def test_add_group(self):
-        wd = self.wd
-        wd.get("http://localhost/addressbook/index.php")
+    def login(self, wd, username, password):
         wd.find_element(By.NAME, "user").click()
         wd.find_element(By.NAME, "user").clear()
-        wd.find_element(By.NAME, "user").send_keys("admin")
+        wd.find_element(By.NAME, "user").send_keys(username)
         wd.find_element(By.ID, "LoginForm").click()
         wd.find_element(By.NAME, "pass").click()
         wd.find_element(By.NAME, "pass").clear()
-        wd.find_element(By.NAME, "pass").send_keys("secret")
+        wd.find_element(By.NAME, "pass").send_keys(password)
         wd.find_element(By.XPATH, "//input[@value='Login']").click()
-        wd.find_element(By.LINK_TEXT, "groups").click()
+    def open_groups_page(self, wd):
+            wd.find_element(By.LINK_TEXT, "groups").click()
+    def create_group(self, wd, group):
+        # init group creation
         wd.find_element(By.NAME, "new").click()
+        # fill group form
         wd.find_element(By.NAME, "group_name").click()
         wd.find_element(By.NAME, "group_name").clear()
-        wd.find_element(By.NAME, "group_name").send_keys("sdsds")
+        wd.find_element(By.NAME, "group_name").send_keys(group.name)
         wd.find_element(By.NAME, "group_header").click()
         wd.find_element(By.NAME, "group_header").clear()
-        wd.find_element(By.NAME, "group_header").send_keys("dfdfd")
+        wd.find_element(By.NAME, "group_header").send_keys(group.header)
         wd.find_element(By.NAME, "group_footer").click()
         wd.find_element(By.NAME, "group_footer").clear()
-        wd.find_element(By.NAME, "group_footer").send_keys("dfdfdf")
+        wd.find_element(By.NAME, "group_footer").send_keys(group.footer)
+        # submit group creation
         wd.find_element(By.NAME, "submit").click()
+
+    def return_to_groups_page(self, wd):
         wd.find_element(By.LINK_TEXT, "group page").click()
+    def logout(self, wd):
         wd.find_element(By.LINK_TEXT, "Logout").click()
-        wd.find_element(By.NAME, "user").clear()
-        wd.find_element(By.NAME, "user").send_keys("admin")
+    def test_add_group(self):
+        wd = self.wd
+        self.open_home_page(wd)
+        self.login(wd, username=  "admin", password="secret")
+        self.open_groups_page(wd)
+        self.create_group(wd, Group(name="sdsds", header="dfdfd", footer="dfdfdf"))
+        self.return_to_groups_page(wd)
+        self.logout(wd)
+
+    def test_add_empty_group(self):
+        wd = self.wd
+        self.open_home_page(wd)
+        self.login(wd, username="admin", password="secret")
+        self.open_groups_page(wd)
+        self.create_group(wd, Group(name="", header="", footer=""))
+        self.return_to_groups_page(wd)
+        self.logout(wd)
+
+    def open_home_page(self, wd):
+        wd.get("http://localhost/addressbook/index.php")
 
     def is_element_present(self, how, what):
         try:
