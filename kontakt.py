d662086ed3f83f87ef0f280f9806d9f7d8e3d592
Violetta Kazantseva <kazanova1931@gmail.com>
2023-06-06 11:12:28 +0300

    Автотест для проверки создания контакта

---
+++ kontakt.py
@@ -0,0 +1,11 @@
+class Kontakt:
+    def __init__(self, first_name, mid_name, last_name, nick_name, title, company_name, address, home_number, e_mail):
+        self.first_name = first_name
+        self.mid_name = mid_name
+        self.last_name = last_name
+        self.nick_name = nick_name
+        self.title = title
+        self.company_name = company_name
+        self.address = address
+        self.home_number = home_number
+        self.e_mail = e_mail
